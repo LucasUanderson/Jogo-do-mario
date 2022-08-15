81654097c60d00a86c968539807afeff7e257ff0
Lucas Uanderson <lucasuanderson159@live.com>
2022-08-15 18:44:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+    
+    },500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+   
+    if( pipePosition <= 72 && pipePosition > 0 &&marioPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = '${pipePosition}px';
+
+        mario.src ="/img/game-over.png";
+        mario.style.width = "55px"
+
+        clearInterval(loop);
+
+    }
+
+
+},10);
+
+document.addEventListener('keydown',jump)
